d15e0c58849e10a0884a3cffa9de329c9afc96a9
Srishailam <sriyd531@gmail.com>
2023-07-01 15:41:08 -0400

    prod and dev config files

--- src/index.js
+++ src/index.js
@@ -6,3 +6,11 @@ helloWorldButton.render();
 
 const heading = new Heading();
 heading.render();
+
+if (process.env.NODE_ENV === 'production') {
+  console.log('Production mode');
+} else if (process.env.NODE_ENV === 'development') {
+  console.log('Development mode');
+}
+
+helloWorldButton.methodThatDoesNotExist();
