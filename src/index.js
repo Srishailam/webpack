141e0c51902852551f4690352cdc3428a36f9bf3
Srishailam <sriyd531@gmail.com>
2023-07-01 00:51:18 -0400

    css loaders for webpack

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { helloWorld } from './hello-world.js'
-import addImage from './add-image.js';
-helloWorld();
-addImage();
+import HelloWorldButton from './components/hello-world-button/hello-world-button.js';
+
+const helloWorldButton = new HelloWorldButton();
+helloWorldButton.render();
