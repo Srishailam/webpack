6b1cf1ce8acb6fd49c90c1412aa8ce99d6dd0548
Srishailam <sriyd531@gmail.com>
2023-07-01 00:58:48 -0400

    scss loaders

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,13 @@ module.exports = {
         use: [
           'style-loader', 'css-loader'
         ]
-      }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader', 'css-loader', 'sass-loader'
+        ],
+      },
     ],
   },
 }
