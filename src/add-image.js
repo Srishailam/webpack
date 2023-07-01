7735656ce71a4e690685929e0b434081c4ff8992
Srishailam <sriyd531@gmail.com>
2023-07-01 00:17:36 -0400

    asset resource type(image loading)

---
+++ src/add-image.js
@@ -0,0 +1,10 @@
+import Potato from './potato.jpg';
+function addImage() {
+  const img = document.createElement('img');
+  img.alt = 'Image';
+  img.width = 300;
+  img.src = Potato;
+  const body = document.querySelector('body');
+  body.appendChild(img);
+}
+export default addImage;
