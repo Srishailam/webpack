d3f50378cb12a11643b56b8683ca32a889be6fac
Srishailam <sriyd531@gmail.com>
2023-07-01 00:35:06 -0400

    asset modules

--- src/add-image.js
+++ src/add-image.js
@@ -1,9 +1,11 @@
 import Potato from './potato.jpg';
+import altText from './potato.txt';
 function addImage() {
   const img = document.createElement('img');
   img.alt = 'Image';
   img.width = 300;
   img.src = Potato;
+  img.alt = altText;
   const body = document.querySelector('body');
   body.appendChild(img);
 }
--- webpack.config.js
+++ webpack.config.js
@@ -4,15 +4,25 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, './dist')
+    path: path.resolve(__dirname, './dist'),
+    publicPath: 'dist/'
   },
   mode: 'none',
   module: {
     rules: [
       {
         test: /\.(png|jpg)$/,
-        type: 'asset/resource'
-      }
+        type: 'asset',
+        parser: {
+          dataUrlCondition: {
+            maxSize: 3 * 1024 // 3kb
+          }
+        }
+      },
+      {
+        test: /\.txt/,
+        type: 'asset/source'
+      },
     ],
   },
 }
